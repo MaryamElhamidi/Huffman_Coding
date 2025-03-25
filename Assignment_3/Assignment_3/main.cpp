b4005660ffb942e4923dc298a532a80d9740a1bb
Janki547 <janx547@gmail.com>
2025-03-25 19:15:44 -0400

    Fixed errors in decoding function.

--- Assignment_3/Assignment_3/main.cpp
+++ Assignment_3/Assignment_3/main.cpp
@@ -59,19 +59,31 @@ HNode* buildHuffmanTree(int freq[256]) {
 
 
 // --------------------------------------------------------
-void decodeCodes(string codes[256], string decodedMessage) {
-    HNode* i = new HNode();
-    for (char c : codes){
-        while(i->character == '\0') {
-            if (c == 0) {
-                i = i->left;
-            }
-            else if (c == 1) {
-                i = i->right;
-            }
+string decodeCodes(const string& code, HNode* root) {
+
+    string decodedText = ""; 
+    *HNode iteratorNode = root;
+
+    for (char bit : code) {
+
+        if (bit == '0')
+        {
+            i = i->left;
         }
+        else if (bit == '1')
+        {
+            i = i->right;
+        }
+
+        if (i->left == nullptr && i->right == nullptr)
+        {
+            decodedText += i->character;
+            i = root;
+        }
+
+        return decodedText;
     }
-    decodedMessage.append(i->character);
+        
 }
 
 int main(int argc, const char * argv[]) {
