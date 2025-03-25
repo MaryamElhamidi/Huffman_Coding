aca13048dc7ee5de4605e530768fcfbd75414348
MaryamElhamidi <unicolmaryame@gmail.com>
2025-03-25 15:20:09 -0400

    Added Comments for File-Handling
    Please note I have decided to use sstream in this assignment. I previously used and explained it in assignment 1.
    
    This is the documentation:
    
    https://stackoverflow.com/questions/35974402/reading-getline-from-cin-into-a-stringstream-c
    
    https://cplusplus.com/reference/sstream/stringstream/stringstream/

--- Assignment_3/Assignment_3/file_handling.cpp
+++ Assignment_3/Assignment_3/file_handling.cpp
@@ -7,25 +7,26 @@
 
 #include "file_handling.h"
 #include <fstream>
-#include <sstream>
+#include <sstream>  // Included sstream to use stringstream for reading file content into a string
 
 string readFile(const string &fileName) {
-    ifstream inputFile(fileName);
-    if (!inputFile.is_open()){
-         return "";
+    ifstream inputFile(fileName);  // Open the file in read mode using an input file stream (ifstream)
+    if (!inputFile.is_open()){     // Check if the file failed to open
+         return ""; // Return an empty string if the file could not be opened
     }
-    stringstream buffer;
-    buffer << inputFile.rdbuf();
+    stringstream buffer; // Create a stringstream object to store file contents
+    buffer << inputFile.rdbuf();  // Read the entire file content into the stringstream buffer
     inputFile.close();
-    return buffer.str();
+    return buffer.str();  // Convert the stringstream content to a string and return it
 }
 
 bool writeFile(const string &fileName, const string &data) {
-    ofstream outputFile(fileName, ios::binary);
-    if (!outputFile.is_open()){
-         return false;
+    ofstream outputFile(fileName, ios::binary);  // Open the file in write mode using an output file stream (ofstream)
+    // Check if the file failed to open
+    if (!outputFile.is_open()) {
+         return false;  // Return false if the file could not be opened for writing
     }
-    outputFile << data;
+    outputFile << data; // Write the provided data string to the file
     outputFile.close();
-    return true;
+    return true;   // Return true to indicate successful writing to the file
 }
