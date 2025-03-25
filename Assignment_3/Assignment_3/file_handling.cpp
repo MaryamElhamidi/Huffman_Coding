afa93111487be42225c47db2c4af51f5a923e697
MaryamElhamidi <unicolmaryame@gmail.com>
2025-03-25 14:33:01 -0400

    File Handling to save the encoded output in a compressed format.

--- Assignment_3/Assignment_3/file_handling.cpp
+++ Assignment_3/Assignment_3/file_handling.cpp
@@ -5,3 +5,27 @@
 //  Created by Maryam Elhamidi on 2025-03-21.
 //
 
+#include "file_handling.h"
+#include <fstream>
+#include <sstream>
+
+string readFile(const string &fileName) {
+    ifstream inputFile(fileName);
+    if (!inputFile.is_open()){
+         return "";
+    }
+    stringstream buffer;
+    buffer << inputFile.rdbuf();
+    inputFile.close();
+    return buffer.str();
+}
+
+bool writeFile(const string &fileName, const string &data) {
+    ofstream outputFile(fileName, ios::binary);
+    if (!outputFile.is_open()){
+         return false;
+    }
+    outputFile << data;
+    outputFile.close();
+    return true;
+}
--- Assignment_3/Assignment_3/file_handling.h
+++ Assignment_3/Assignment_3/file_handling.h
@@ -5,3 +5,18 @@
 //  Created by Maryam Elhamidi on 2025-03-21.
 //
 
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
+#include <string>
+using namespace std;
+
+// Reads the entire content of the specified file and returns it as a string.
+// Returns an empty string if the file cannot be opened.
+string readFile(const string &fileName);
+
+// Writes the provided data to the specified file in binary mode.
+// Returns true if writing was successful, false otherwise.
+bool writeFile(const string &fileName, const string &data);
+
+#endif
