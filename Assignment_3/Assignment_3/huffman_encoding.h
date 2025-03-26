1d391e1e4e408680fee00458b142c3d91ac33e85
MaryamElhamidi <unicolmaryame@gmail.com>
2025-03-25 22:02:47 -0400

    Moved Encoding Implementation to Seperate Files

--- Assignment_3/Assignment_3/huffman_encoding.h
+++ Assignment_3/Assignment_3/huffman_encoding.h
@@ -4,3 +4,18 @@
 //
 //  Created by Maryam Elhamidi on 2025-03-21.
 //
+#ifndef HUFFMAN_ENCODING_H
+#define HUFFMAN_ENCODING_H
+
+#include "huffman_tree.cpp"
+#include <string>
+
+using namespace std;
+
+// Function to generate Huffman codes from the Huffman tree
+void generateCodes(HNode* root, const string &code, string codes[256]);
+
+// Function to build a Huffman tree from a character frequency table
+HNode* buildHuffmanTree(int freq[256]);
+
+#endif
