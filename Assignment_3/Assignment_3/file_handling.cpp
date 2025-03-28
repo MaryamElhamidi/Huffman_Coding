37f398bd3d42a56500e4fe44fe086e36e72b466f
MaryamElhamidi <unicolmaryame@gmail.com>
2025-03-28 03:31:13 -0400

    Handled cases such as file not found, not able to write/read, etc

--- Assignment_3/Assignment_3/file_handling.cpp
+++ Assignment_3/Assignment_3/file_handling.cpp
@@ -6,13 +6,15 @@
 //
 
 #include "file_handling.h"
+#include <iostream>
 #include <fstream>
 #include <sstream>  // Included sstream to use stringstream for reading file content into a string
 
 string readFile(const string &fileName) {
     ifstream inputFile(fileName);  // Open the file in read mode using an input file stream (ifstream)
     if (!inputFile.is_open()){     // Check if the file failed to open
-         return ""; // Return an empty string if the file could not be opened
+        cerr << "Error Opening File: " <<fileName << endl;
+        return ""; // Return an empty string if the file could not be opened
     }
     stringstream buffer; // Create a stringstream object to store file contents
     buffer << inputFile.rdbuf();  // Read the entire file content into the stringstream buffer
@@ -24,7 +26,8 @@ bool writeFile(const string &fileName, const string &data) {
     ofstream outputFile(fileName, ios::binary);  // Open the file in write mode using an output file stream (ofstream)
     // Check if the file failed to open
     if (!outputFile.is_open()) {
-         return false;  // Return false if the file could not be opened for writing
+        cerr << "Error writing to file: " <<fileName << endl;
+        return false;  // Return false if the file could not be opened for writing
     }
     outputFile << data; // Write the provided data string to the file
     outputFile.close();
